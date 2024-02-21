76e9ea984a94f82a58b719da9f48c55d379823e4
BrandonJung <49972155+BrandonJung@users.noreply.github.com>
2024-02-20 23:39:17 -0800

    Added bottom tabs

--- App.js
+++ App.js
@@ -2,14 +2,34 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import HomePage from './src/pages/HomePage';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName='HomePage'
+      screenOptions={{ headerShown: true }}>
+      <Tab.Screen
+        name='HomePage'
+        component={HomePage}
+        options={({ navigation, route }) => ({ title: 'Home' })}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home' component={HomePage} />
+      <Stack.Navigator initialRouteName='TabNavigator'>
+        <Stack.Screen
+          name='TabNavigator'
+          component={TabNavigator}
+          options={({ navigation, route }) => ({ headerShown: false })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 
-const HomePage = () => {
+const HomePage = ({ navigation }) => {
   return (
     <View>
       <Text>HomePage</Text>
