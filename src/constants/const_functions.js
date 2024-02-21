82101493ee8ac142856e3d4703eaddc8828bf8d6
BrandonJung <49972155+BrandonJung@users.noreply.github.com>
2024-02-21 00:31:16 -0800

    Added async storage + async functions + homepage add button

---
+++ src/constants/const_functions.js
@@ -0,0 +1,34 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeData = async (key, value) => {
+  const convertedValue = JSON.stringify(value);
+  try {
+    const storeRes = await AsyncStorage.setItem(key, convertedValue);
+    return storeRes;
+  } catch (e) {
+    console.log('Error storing data', e);
+  }
+};
+
+export const RemoveFromStorage = async (key) => {
+  try {
+    const removeRes = await AsyncStorage.removeItem(key);
+    return removeRes;
+  } catch (e) {
+    console.log('Error removing data', e);
+  }
+};
+
+export const GetFromStorage = async (key) => {
+  try {
+    const getRes = await AsyncStorage.getItem(key);
+    if (getRes) {
+      const convertedRetValue = JSON.parse(getRes);
+      return convertedRetValue;
+    } else {
+      return null;
+    }
+  } catch (e) {
+    console.log('Error getting data', e);
+  }
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { FlatList, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { TestRestaurants } from '../dummyData';
 import RestaurantCard from '../components/RestaurantCard';
 
 const HomePage = ({ navigation }) => {
+  const [showOptions, setShowOptions] = useState(false);
   return (
-    <View>
+    <View style={{ flex: 1 }}>
       <FlatList
         data={TestRestaurants}
         renderItem={({ item, index }) => {
@@ -18,8 +25,49 @@ const HomePage = ({ navigation }) => {
           );
         }}
       />
+      <View>
+        {showOptions ? (
+          <View style={styles.addButtonOptionContainer}>
+            <TouchableOpacity style={styles.addButtonOption} onPress={() => {}}>
+              <Text>Add Restaurant</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.addButtonOption} onPress={() => {}}>
+              <Text>Add Food</Text>
+            </TouchableOpacity>
+          </View>
+        ) : null}
+        <TouchableOpacity
+          style={styles.addButton}
+          onPress={() => setShowOptions(!showOptions)}>
+          <Text style={{ fontSize: 20 }}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  addButton: {
+    position: 'absolute',
+    bottom: 16,
+    right: 16,
+    backgroundColor: 'lightblue',
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addButtonOptionContainer: {
+    position: 'absolute',
+    bottom: 36,
+    right: 36,
+    backgroundColor: 'lightblue',
+    paddingBottom: 20,
+    padding: 6,
+    borderRadius: 6,
+  },
+  addButtonOption: { padding: 6 },
+});
 
 export default HomePage;
