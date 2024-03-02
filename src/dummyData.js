627058279da9478ed03fc60f7294b115bb45c04f
BrandonJung <49972155+BrandonJung@users.noreply.github.com>
2024-03-02 06:19:51 -0800

    Update dummyData.js

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,34 +1,29 @@
-export const TestRestaurants = [
+const restaurantData = [
   {
-    id: 0,
-    key: 'rest_0',
-    name: "Trevor's Restaurant",
-    imageURL: '',
     foodList: [
       {
+        eater: ['Brandon'],
         id: 0,
+        imageURL:
+          'file:///Users/brandon/Library/Developer/CoreSimulator/Devices/05B60A52-4274-4E75-B984-B1FA89FF7940/data/Containers/Data/Application/A671CA1E-9F76-4A03-A9AF-D14DE4A1E075/tmp/D46F2F97-366C-453D-8E0E-BAB0257B251F.jpg',
         key: 'food_0',
-        name: 'Corn Cheese',
-        eater: 'Trevor',
-        stars: null,
-        note: '',
-      },
-      {
-        id: 1,
-        key: 'food_1',
-        name: 'Burgers',
-        eater: 'Trevor',
-        stars: null,
-        note: '',
-      },
-      {
-        id: 2,
-        key: 'food_2',
-        name: 'Pho Bowl',
-        eater: 'Brandon',
-        stars: 4,
-        note: '',
+        name: 'Cheese',
+        notes: [
+          { foodName: 'Cheese', name: 'Brandon', note: 'This was so good' },
+          { foodName: 'Cheese', name: 'Brandon', note: 'This was bad the second time' },
+          { name: 'Brandon', note: 'Had this with half sugar, was better' },
+        ],
+        ratings: [
+          { foodName: 'Cheese', name: 'Brandon', rating: 3 },
+          { foodName: 'Cheese', name: 'Brandon', rating: 2 },
+          { name: 'Brandon', rating: 5 },
+        ],
       },
     ],
+    id: 0,
+    imageURL:
+      'file:///Users/brandon/Library/Developer/CoreSimulator/Devices/05B60A52-4274-4E75-B984-B1FA89FF7940/data/Containers/Data/Application/A671CA1E-9F76-4A03-A9AF-D14DE4A1E075/tmp/FFC0DCD7-B0E9-4F76-A551-21FD310C9683.jpg',
+    key: 'rest_0',
+    name: 'Elisas',
   },
 ];
