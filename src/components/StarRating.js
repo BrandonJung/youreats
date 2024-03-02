e148f62fa4c452bffd6cf4242a97d7094d009fa8
BrandonJung <49972155+BrandonJung@users.noreply.github.com>
2024-03-02 03:45:00 -0800

    Faster rendering of star buttons

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -8,63 +8,29 @@ const marginLeftAmount = 16;
 const StarRating = ({ rating, setRating }) => {
   const filledStarURL = 'https://youreats.s3.amazonaws.com/icons/star-filled.svg';
 
+  const starButton = (passedRating) => {
+    return (
+      <TouchableOpacity onPress={() => setRating(passedRating)}>
+        <SvgWithCssUri
+          uri={filledStarURL}
+          width={starRatingSize}
+          height={starRatingSize}
+          fill={rating >= passedRating ? 'yellow' : '#F6F6F6'}
+          stroke={'black'}
+          strokeWidth={2}
+          style={{ marginLeft: marginLeftAmount }}
+        />
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View style={{ flexDirection: 'row', justifyContent: 'center', marginTop: 8 }}>
-      <TouchableOpacity onPress={() => setRating(1)}>
-        <SvgWithCssUri
-          uri='https://youreats.s3.amazonaws.com/icons/star-filled.svg'
-          width={starRatingSize}
-          height={starRatingSize}
-          fill={rating >= 1 ? 'yellow' : '#F6F6F6'}
-          stroke={'black'}
-          strokeWidth={2}
-          style={{ marginLeft: marginLeftAmount }}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setRating(2)}>
-        <SvgWithCssUri
-          uri='https://youreats.s3.amazonaws.com/icons/star-filled.svg'
-          width={starRatingSize}
-          height={starRatingSize}
-          fill={rating >= 2 ? 'yellow' : '#F6F6F6'}
-          stroke={'black'}
-          strokeWidth={2}
-          style={{ marginLeft: marginLeftAmount }}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setRating(3)}>
-        <SvgWithCssUri
-          uri='https://youreats.s3.amazonaws.com/icons/star-filled.svg'
-          width={starRatingSize}
-          height={starRatingSize}
-          fill={rating >= 3 ? 'yellow' : '#F6F6F6'}
-          stroke={'black'}
-          strokeWidth={2}
-          style={{ marginLeft: marginLeftAmount }}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setRating(4)}>
-        <SvgWithCssUri
-          uri={filledStarURL}
-          width={starRatingSize}
-          height={starRatingSize}
-          fill={rating >= 4 ? 'yellow' : '#F6F6F6'}
-          stroke={'black'}
-          strokeWidth={2}
-          style={{ marginLeft: marginLeftAmount }}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => setRating(5)}>
-        <SvgWithCssUri
-          uri={filledStarURL}
-          width={starRatingSize}
-          height={starRatingSize}
-          fill={rating === 5 ? 'yellow' : '#F6F6F6'}
-          stroke={'black'}
-          strokeWidth={2}
-          style={{ marginLeft: marginLeftAmount }}
-        />
-      </TouchableOpacity>
+      {starButton(1)}
+      {starButton(2)}
+      {starButton(3)}
+      {starButton(4)}
+      {starButton(5)}
     </View>
   );
 };
