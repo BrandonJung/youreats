f232f6231de438f576d7fe0344337a0dde998df4
BrandonJung <49972155+BrandonJung@users.noreply.github.com>
2024-03-13 05:37:50 -0700

    Updated donald duck

--- src/donaldDuck.js
+++ src/donaldDuck.js
@@ -1,37 +1,38 @@
-const restaurantData = [
-  {
-    foodList: [
-      {
-        eater: ['Brandon'],
-        id: 0,
-        imageURL:
-          'file:///Users/brandon/Library/Developer/CoreSimulator/Devices/05B60A52-4274-4E75-B984-B1FA89FF7940/data/Containers/Data/Application/A671CA1E-9F76-4A03-A9AF-D14DE4A1E075/tmp/D46F2F97-366C-453D-8E0E-BAB0257B251F.jpg',
-        key: 'food_0',
-        name: 'Cheese',
-        notes: [
-          { foodName: 'Cheese', name: 'Brandon', note: 'This was so good' },
-          { foodName: 'Cheese', name: 'Brandon', note: 'This was bad the second time' },
-          { name: 'Brandon', note: 'Had this with half sugar, was better' },
-        ],
-        ratings: [
-          { foodName: 'Cheese', name: 'Brandon', rating: 3 },
-          { foodName: 'Cheese', name: 'Brandon', rating: 2 },
-          { name: 'Brandon', rating: 5 },
-        ],
-      },
-    ],
-    id: 0,
-    imageURL:
-      'file:///Users/brandon/Library/Developer/CoreSimulator/Devices/05B60A52-4274-4E75-B984-B1FA89FF7940/data/Containers/Data/Application/A671CA1E-9F76-4A03-A9AF-D14DE4A1E075/tmp/FFC0DCD7-B0E9-4F76-A551-21FD310C9683.jpg',
-    key: 'rest_0',
-    name: 'Elisas',
+const restaurantData = {
+  _id: {
+    $oid: '65f199db759d20ea85ad9b1a',
   },
-];
+  name: 'Elisa',
+  imageURL: '',
+  foodListId: {
+    $oid: '65f19b1e759d20ea85ad9b1b',
+  },
+  usersSubscribed: ['65f175f1ed20e2cc61c6b392'],
+};
 
-const user = {
+const foodsListData = {
+  _id: {
+    $oid: '65f19c67759d20ea85ad9b1d',
+  },
+  foods: [
+    {
+      name: 'Cheese',
+      restaurantId: {
+        $oid: '65f19c5c759d20ea85ad9b1c',
+      },
+      eaters: ['Brandon'],
+      ratings: [],
+      notes: [],
+    },
+  ],
+};
+
+const userData = {
+  _id: {
+    $oid: '65f1591e14d870e1c6ad6c61',
+  },
   firstName: 'Brandon',
+  email: '7788382328',
   lastName: 'Jung',
-  id: 'uuid',
-  email: 'brandonjung111@gmail.com',
-  mobile: '7788382328',
+  mobile: 'Brandonjung111@gmail.com',
 };
