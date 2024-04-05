1a0af894aa60c61e1d7c6de508afd7e8fb379df0
BrandonJung <49972155+BrandonJung@users.noreply.github.com>
2024-04-05 13:39:04 -0400

    Added homepage different views

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
-import { Alert, Dimensions, FlatList, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert, Dimensions, FlatList, Text, TouchableOpacity, View } from 'react-native';
 import RestaurantCard from '../components/RestaurantCard';
 import _ from 'lodash';
 import OptionsPopup from '../components/OptionsPopup';
 import OptionsButton from '../components/OptionsButton';
 import { useRestaurant } from '../contexts/Restaurant';
 import { useUser } from '../contexts/User';
+import { SvgWithCssUri } from 'react-native-svg/css';
+import RestaurantListItem from '../components/RestaurantListItem';
+import SearchBar from '../components/SearchBar';
 
 const { width, height } = Dimensions.get('window');
 const cardWidth = 160;
 const cardGap = (width - 2 * cardWidth) / 3;
 
+const viewIconSize = 30;
+
 const HomePage = ({ navigation }) => {
   const [showOptions, setShowOptions] = useState(false);
+  const [viewStyle, setViewStyle] = useState(1);
+  const [searchValue, setSearchValue] = useState('');
 
   const { restaurantsData, addRestaurant } = useRestaurant();
+  const [restaurantList, setRestaurantList] = useState(null);
   const { userData } = useUser();
 
   const handleAddRestaurant = (restaurantName) => {
@@ -34,24 +42,111 @@ const HomePage = ({ navigation }) => {
     addRestaurant(restaurantName);
   };
 
+  useEffect(() => {
+    if (restaurantsData?.length > 0) {
+      setRestaurantList(restaurantsData);
+    }
+  }, [restaurantsData]);
+
+  const handleSearch = (passedSearchValue) => {
+    if (passedSearchValue === null || passedSearchValue === '') {
+      setRestaurantList(restaurantsData);
+      return;
+    }
+    const lowerSearchValue = passedSearchValue.toLowerCase();
+    const restaurantListClone = _.cloneDeep(restaurantList);
+    const retRestaurantArray = [];
+    for (const restaurantObj of restaurantListClone) {
+      if (restaurantObj.name.toLowerCase().includes(lowerSearchValue))
+        retRestaurantArray.push(restaurantObj);
+      continue;
+    }
+    setRestaurantList(retRestaurantArray);
+  };
+
+  useEffect(() => {
+    handleSearch(searchValue);
+  }, [searchValue]);
+
   return (
     <View style={{ flex: 1 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          // justifyContent: 'flex-end',
+          alignItems: 'center',
+          marginTop: 20,
+          marginRight: 20,
+        }}>
+        <View style={{ flex: 1 }}>
+          <SearchBar
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+            placeholderText='Search here'
+          />
+        </View>
+        <TouchableOpacity
+          onPress={() => setViewStyle(0)}
+          style={[
+            { marginLeft: 10, padding: 4, borderRadius: 6 },
+            viewStyle === 0 ? { backgroundColor: '#FFFFFF' } : {},
+          ]}>
+          <SvgWithCssUri
+            uri={`https://youreats.s3.amazonaws.com/icons/list.svg`}
+            width={viewIconSize}
+            height={viewIconSize}
+            fill={'#bebebe'}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => setViewStyle(1)}
+          style={[
+            { marginLeft: 10, padding: 4, borderRadius: 6 },
+            viewStyle === 1 ? { backgroundColor: '#FFFFFF' } : {},
+          ]}>
+          <SvgWithCssUri
+            uri={`https://youreats.s3.amazonaws.com/icons/menu.svg`}
+            width={viewIconSize}
+            height={viewIconSize}
+            fill={'#bebebe'}
+          />
+        </TouchableOpacity>
+      </View>
       {restaurantsData?.length > 0 ? (
-        <FlatList
-          data={restaurantsData}
-          numColumns={2}
-          columnWrapperStyle={{ columnGap: cardGap, marginHorizontal: cardGap }}
-          renderItem={({ item, index }) => {
-            return (
-              <RestaurantCard
-                restaurant={item}
-                navigation={navigation}
-                cardWidth={cardWidth}
-                setShowOptions={setShowOptions}
-              />
-            );
-          }}
-        />
+        viewStyle === 0 ? (
+          <FlatList
+            key={'restaurant-list-1'}
+            contentContainerStyle={{ marginHorizontal: 10, marginTop: 10 }}
+            data={restaurantList}
+            renderItem={({ item, index }) => {
+              return (
+                <RestaurantListItem
+                  restaurant={item}
+                  navigation={navigation}
+                  cardWidth={cardWidth}
+                  setShowOptions={setShowOptions}
+                />
+              );
+            }}
+          />
+        ) : (
+          <FlatList
+            key={'restaurant-list-2'}
+            data={restaurantList}
+            numColumns={2}
+            columnWrapperStyle={{ columnGap: cardGap, marginHorizontal: cardGap }}
+            renderItem={({ item, index }) => {
+              return (
+                <RestaurantCard
+                  restaurant={item}
+                  navigation={navigation}
+                  cardWidth={cardWidth}
+                  setShowOptions={setShowOptions}
+                />
+              );
+            }}
+          />
+        )
       ) : (
         <Text style={{ flex: 1 }}>Add a restaurant to start!</Text>
       )}
